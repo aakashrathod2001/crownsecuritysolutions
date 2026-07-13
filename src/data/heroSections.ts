cd02c5be2379726257819bfff55a2a2252b7eef8
devtitus <m.works.gd@gmail.com>
2026-07-13 21:06:32 +0530

    update: hero image

--- src/data/heroSections.ts
+++ src/data/heroSections.ts
@@ -4,7 +4,7 @@ import { HeroSectionProps } from "@/types/hero";
 export const homepageHero: HeroSectionProps = {
   media: {
     type: "image",
-    src: "/images/home/home-hero-bg.webp",
+    src: "/images/home/home_new.webp",
     alt: "Security guard at concert",
   },
   content: {
