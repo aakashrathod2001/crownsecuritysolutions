9674ba44f85000f1a5b20400c2c14b624777e1d1
devtitus <m.works.gd@gmail.com>
2026-08-21 13:23:53 +0530

    refractor: Hospital Hero Image updated

--- src/data/services.ts
+++ src/data/services.ts
@@ -464,11 +464,11 @@ export const services: ServiceData[] = [
       "Emergency response and evacuation procedures.",
       "24/7 front desk and concierge support.",
     ],
-    image: "/images/services/Hospitality Services/h1.webp",
+    image: "/images/services/Hospitality Services/hospital_hero_image.webp",
     heroData: {
       media: {
         type: "image",
-        src: "/images/services/Hospitality Services/h1.webp",
+        src: "/images/services/Hospitality Services/hospital_hero_image.webp",
         alt: "Hospitality Services",
       },
       content: {
